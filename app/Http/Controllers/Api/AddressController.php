d4dc3dc0d12f725f6b52f5b9119e50e4a44316cd
ThiagoFTelles <atendimento.telles@gmail.com>
2022-08-16 16:36:13 -0300

    Initial commit

---
+++ app/Http/Controllers/Api/AddressController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAddressRequest;
+use App\Models\Address;
+use Illuminate\Http\Request;
+
+use function GuzzleHttp\Promise\all;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $addresses = Address::all();
+        
+        return response()->json([
+            'status' => true,
+            'addresses' => $addresses
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAddressRequest $request)
+    {
+        $address = Address::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Address created succesfully.',
+            'address' => $address
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Address $address)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Address $address)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreAddressRequest $request, Address $address)
+    {
+        $address->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Address updated succesfully.',
+            'address' => $address
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Address  $address
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Address $address)
+    {
+        $address->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Address deleted succesfully.'
+        ], 200);
+    }
+}
---
+++ app/Http/Requests/StoreAddressRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "postal_code" => "required|max:8",
+            "street" => "required|max:70",
+            "number" => "max:10",
+            "complement" => "max:70",
+            "district" => "required|max:70",
+            "city" => "required|max:70",
+            "state" => "required|max:70",
+            "country" => "required|max:70",
+        ];
+    }
+}
